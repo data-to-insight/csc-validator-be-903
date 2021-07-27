b3fd02ee1cddac9d9f47f2de772a792b044a58e8
Mark Waddoups <mark.waddoups@socialfinance.org.uk>
2021-07-27 10:53:28 +0100

    Added documentation for ErrorDescription and made sure functions return lists

--- validator903/types.py
+++ validator903/types.py
@@ -3,6 +3,14 @@ from typing import List, TypedDict
 
 @dataclass
 class ErrorDefinition:
+    """
+    Error definition information that is passed onto the frontend tool. The code and description are used for display, 
+    and the affected_fields is a list of fields which will be highlighted by the frontend tool if present.
+
+    :param code: String describing the error code e.g. '103'
+    :param description: String describing the error (from guidance) e.g. 'The ethnicity code is either not valid or has not been entered'
+    :param affected_fields: A list of fields to highlight in the tool e.g. ['ETHNIC']
+    """
     code: str
     description: str
     affected_fields: List[str]
--- validator903/validators.py
+++ validator903/validators.py
@@ -3,7 +3,7 @@ from .types import ErrorDefinition
 
 def fake_error():
     error = ErrorDefinition(
-        code=1003,
+        code='1003',
         description='A fake error that fires if the child was born prior to 2006',
         affected_fields=['DOB'],
     )
@@ -11,7 +11,7 @@ def fake_error():
     def _validate(dfs):
         header = dfs['Header']
         mask = pd.to_datetime(header['DOB'], format='%d/%m/%Y').dt.year <= 2006
-        return {'Header': header.index[mask].values}
+        return {'Header': header.index[mask].tolist()}
     
     return error, _validate
 
@@ -25,6 +25,6 @@ def fake_error2():
     def _validate(dfs):
         df = dfs['Episodes']
         mask = df['HOME_POST'].str.contains('F')
-        return {'Episodes': df.index[mask].values}
+        return {'Episodes': df.index[mask].tolist()}
 
     return error, _validate

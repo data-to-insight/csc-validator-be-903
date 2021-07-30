50e95274b4648bce3a9e5f923b517f67c0ba13d7
Mark Waddoups <mwaddoups@gmail.com>
2021-07-30 07:26:33 +0000

    Added the 101 validation rule.

--- validator903/validators.py
+++ validator903/validators.py
@@ -1,6 +1,29 @@
 import pandas as pd
 from .types import ErrorDefinition
 
+def validate_101():
+    error = ErrorDefinition(
+        code='101',
+        description='Gender code is not valid.',
+        affected_fields=['SEX'],
+    )
+
+    def _validate(dfs):
+        if 'Header' not in dfs:
+            return {}
+        
+        header = dfs['Header']
+        code_list = [1, 2]
+
+        mask = header['SEX'].isin(code_list)
+
+        validation_error_mask = ~mask
+        validation_error_locations = header.index[validation_error_mask]
+
+        return {'Header': validation_error_locations.tolist()}
+
+    return error, _validate
+
 def fake_error():
     error = ErrorDefinition(
         code='1003',

36754fa7cd2972becf2adc82e470550f888159a9
Fan Jiayang <>
2021-08-09 14:01:50 +0000

    Added the 103 validation rule.

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,6 +1,19 @@
 from validator903.validators import *
 import pandas as pd
 
+def test_validate_103():
+    fake_data = pd.DataFrame({
+        'ETHNIC': ['WBRI', 'WIRI', 'WOTH', 'wbri', 'White British', '', pd.NA],
+    })
+
+    fake_dfs = {'Header': fake_data}
+
+    error_defn, error_func = validate_103()
+
+    result = error_func(fake_dfs)
+
+    assert result == {'Header': [3, 4, 5, 6]}
+
 def test_validate_101():
     fake_data = pd.DataFrame({
         'SEX': [1, 2, 3, pd.NA],

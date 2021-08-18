448790728f6f8057ece3872db11fd5bae259db7e
Mark Waddoups <mark.waddoups@socialfinance.org.uk>
2021-08-18 14:32:29 +0100

    Fixed passing metadata via the API

--- validator903/api.py
+++ validator903/api.py
@@ -1,3 +1,4 @@
+from copy import copy
 from collections import defaultdict
 from dataclasses import asdict
 from typing import Any, List, Dict
@@ -19,11 +20,12 @@ def run_validation_for_javascript(uploaded_files: List[UploadedFile], error_code
       - error_definitions - A nested dictionary of {file name -> index in file -> list of error codes} 
     """
     dfs = read_from_text(raw_files=uploaded_files)
-    dfs['metadata'] = metadata
         
     js_files = {k: [t._asdict() for t in df.itertuples(index=True)] for k, df in dfs.items()}
 
-    validated = [(error, f(dfs)) for error, f in configured_errors if error.code in error_codes]
+    handle = copy(dfs)
+    handle['metadata'] = metadata
+    validated = [(error, f(handle)) for error, f in configured_errors if error.code in error_codes]
 
     # Passed to JS
     error_definitions = {e.code: asdict(e) for e, _ in validated}

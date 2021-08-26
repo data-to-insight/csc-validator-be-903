e013b93b5feefafff496ca82d72763846d5d4d7e
Fan Jiayang <jfan2006@gmail.com>
2021-08-26 10:28:21 +0000

    Changed variable name for code lists, broken into three lines for mask.

--- validator903/validators.py
+++ validator903/validators.py
@@ -13,11 +13,17 @@ def validate_178():
             return {}
         
         episodes = dfs['Episodes']
-        code_list_1 = ['PR0', 'PR1', 'PR2', 'PR3', 'PR4', 'PR5']
-        code_list_2 = ['T0', 'T1', 'T2', 'T3', 'T4', 'Z1']
+        code_list_placement_provider = ['PR0', 'PR1', 'PR2', 'PR3', 'PR4', 'PR5']
+        code_list_placement_with_no_provider = ['T0', 'T1', 'T2', 'T3', 'T4', 'Z1']
 
-        mask = episodes['PLACE_PROVIDER'].isin(code_list_1) & ~episodes['PLACE'].isin(code_list_2) | episodes['PLACE_PROVIDER'].isna() | episodes['PLACE_PROVIDER'].isna() & episodes['PLACE'].isin(code_list_2) 
+        place_provider_needed_and_correct = episodes['PLACE_PROVIDER'].isin(code_list_placement_provider) & ~episodes['PLACE'].isin(code_list_placement_with_no_provider) 
         
+        place_provider_not_provided = episodes['PLACE_PROVIDER'].isna() 
+        
+        place_provider_not_needed = episodes['PLACE_PROVIDER'].isna() & episodes['PLACE'].isin(code_list_placement_with_no_provider) 
+        
+        mask = place_provider_needed_and_correct | place_provider_not_provided | place_provider_not_needed
+
         validation_error_mask = ~mask
         validation_error_locations = episodes.index[validation_error_mask]
 

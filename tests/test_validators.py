9026de8e20cf2370d097cd5a5721bd7016b00078
Phil Deakin <89073292+PhilDeakin@users.noreply.github.com>
2021-09-30 12:36:55 +0100

    Update test_validators.py

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -977,7 +977,7 @@ def test_validate_567():
     assert result == {'Missing': [2,3]}
 
 def test_validate_304():
-    fake_mis = pd.DataFrame([
+    fake_uasc = pd.DataFrame([
     { 'DOB' : '01/06/2000', 'DUC' : '05/06/2019' }, #0 Fails 
     { 'DOB' : '02/06/2000', 'DUC' : pd.NA },        #1
     { 'DOB' : '03/06/2000', 'DUC' : '01/06/2015' }, #2
@@ -985,7 +985,7 @@ def test_validate_304():
     { 'DOB' : pd.NA,        'DUC' : '05/06/2020' }, #4  
     ])
 
-    fake_dfs = {'UASC': fake_mis}
+    fake_dfs = {'UASC': fake_uasc}
 
     error_defn, error_func = validate_304()
 

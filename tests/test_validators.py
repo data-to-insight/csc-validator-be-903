a7d2cdfe6db9064b0dda31c8fb4a0bfa5ce2f4e5
dezog <LEVEl3399>
2021-11-11 13:24:52 +0000

    Fixed date handling some more

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -94,8 +94,8 @@ def test_validate_365():
 
 def test_validate_367():
     test_eps = pd.DataFrame([
-        {'CHILD': 101, 'DECOM': '01/01/2000', 'DEC': '01/06/2000', 'LS': 'V3'}, #0 Fail!
-        {'CHILD': 2002, 'DECOM': '01/01/2000', 'DEC': '01/05/2000', 'LS': 'V3'}, #1
+        {'CHILD': 101, 'DECOM': '01/01/2000', 'DEC': '01/06/2000', 'LS': 'V3'}, #0 decom-->col_start
+        {'CHILD': 2002, 'DECOM': '01/01/2000', 'DEC': '01/05/2000', 'LS': 'V3'}, #1 decom-->col_start
         {'CHILD': 30003, 'DECOM': '01/04/2000', 'DEC': '01/05/2000', 'LS': 'V3'}, #2 ^ Fail!
         {'CHILD': 30003, 'DECOM': '01/05/2000', 'DEC': '01/07/2000', 'LS': 'xx'}, #3 |
         {'CHILD': 30003, 'DECOM': '01/06/2000', 'DEC': '01/07/2000', 'LS': 'V3'}, #4 | Fail!
@@ -105,17 +105,18 @@ def test_validate_367():
         {'CHILD': 555, 'DECOM': '01/07/2000', 'DEC': pd.NA, 'LS': 'V3'}, #8 Fail!
     ])
 
-    test_meta = {'collection_start': '01/04/2000'}
+    test_meta = {'collection_start': '01/04/2000',
+                 'collection_end': '01/04/2001'}
 
     test_dfs = {
         'Episodes': test_eps,
         'metadata': test_meta}
 
-    error_defn, error_func = validate_366()
+    error_defn, error_func = validate_367()
 
     result = error_func(test_dfs)
 
-    assert result == {'Episodes': [0, 2, 4, 5, 7, 8]}
+    assert result == {'Episodes': [2, 4, 5, 7, 8]}
 
 
 def test_validate_440():
--- validator903/validators.py
+++ validator903/validators.py
@@ -132,16 +132,18 @@ def validate_367():
         episodes = dfs['Episodes']
         V3_eps = episodes[episodes['LS'] == 'V3']
 
+        V3_eps = V3_eps.dropna(subset=['DECOM']) # missing DECOM should get fixed before looking for this error
+
         collection_start = pd.to_datetime(dfs['metadata']['collection_start'], format='%d/%m/%Y', errors='coerce')
         collection_end = pd.to_datetime(dfs['metadata']['collection_end'], format='%d/%m/%Y', errors='coerce')
-        V3_eps['DECOM'] = pd.to_datetime(V3_eps['DECOM'], format='%d/%m/%Y', errors='coerce')
-        V3_eps['DEC'] = pd.to_datetime(V3_eps['DEC'], format='%d/%m/%Y', errors='coerce')
+        V3_eps['DECOM_dt'] = pd.to_datetime(V3_eps['DECOM'], format='%d/%m/%Y', errors='coerce')
+        V3_eps['DEC_dt'] = pd.to_datetime(V3_eps['DEC'], format='%d/%m/%Y', errors='coerce')
 
         # truncate episode start/end dates to collection start/end respectively
-        V3_eps.loc[V3_eps['DEC'].isna() | (V3_eps['DEC'] > collection_end), 'DEC'] = collection_end
-        V3_eps.loc[V3_eps['DECOM'].isna() | (V3_eps['DECOM'] < collection_start), 'DECOM'] = collection_start
+        V3_eps.loc[V3_eps['DEC'].isna() | (V3_eps['DEC_dt'] > collection_end), 'DEC_dt'] = collection_end
+        V3_eps.loc[V3_eps['DECOM_dt'] < collection_start, 'DECOM_dt'] = collection_start
 
-        V3_eps['duration'] = (V3_eps['DEC'] - V3_eps['DECOM']).dt.days
+        V3_eps['duration'] = (V3_eps['DEC_dt'] - V3_eps['DECOM_dt']).dt.days
         V3_eps = V3_eps[V3_eps['duration'] > 0]
 
         V3_eps['year_total_duration'] = V3_eps.groupby('CHILD')['duration'].transform(sum)

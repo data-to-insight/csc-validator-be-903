5edd926d5683c826231c391ea2a2afab970cb2c3
Phil Deakin <philip.deakin@manchester.gov.uk>
2021-11-15 13:26:19 +0000

    Added 370,371,372,373,374,375,376,379

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -2700,3 +2700,28 @@ def test_validate_529():
     result = error_func(fake_dfs)
 
     assert result == {'Episodes': [2,5]}
+
+def test_validate_383():
+    fake_data = pd.DataFrame([
+        {'CHILD': '111', 'RNE': 'S', 'DECOM': '01/06/2020', 'PLACE': 'S1'},  # 0
+        {'CHILD': '111', 'RNE': 'S', 'DECOM': '05/06/2020', 'PLACE': 'T1'},  # 1 Middle Fails as next RNE not P
+        {'CHILD': '111', 'RNE': 'T', 'DECOM': '06/06/2020', 'PLACE': 'S1'},  # 2
+        {'CHILD': '222', 'RNE': 'S', 'DECOM': '05/06/2020', 'PLACE': 'T1'},  # 3
+        {'CHILD': '333', 'RNE': 'S', 'DECOM': '06/06/2020', 'PLACE': 'S1'},  # 4
+        {'CHILD': '333', 'RNE': 'S', 'DECOM': '10/06/2020', 'PLACE': 'T1'},  # 5 Middle Fails as pre PL not next PL
+        {'CHILD': '333', 'RNE': 'P', 'DECOM': '12/06/2020', 'PLACE': 'S2'},  # 6
+        {'CHILD': '444', 'RNE': 'S', 'DECOM': '08/06/2020', 'PLACE': 'T1'},  # 7
+        {'CHILD': '444', 'RNE': 'S', 'DECOM': '09/06/2020', 'PLACE': 'P4'},  # 8  Middle Passes not a T code
+        {'CHILD': '444', 'RNE': 'S', 'DECOM': '15/06/2020', 'PLACE': 'T1'},  # 9
+        {'CHILD': '6666', 'RNE': 'S', 'DECOM': '12/06/2020', 'PLACE': 'T1'},  # 10
+        {'CHILD': '6666', 'RNE': 'S', 'DECOM': '13/06/2020', 'PLACE': 'T1'},  # 11 Middle Passes
+        {'CHILD': '6666', 'RNE': 'P', 'DECOM': '14/06/2020', 'PLACE': 'T1'},  # 12
+    ])
+
+    fake_dfs = {'Episodes': fake_data}
+
+    error_defn, error_func = validate_383()
+
+    result = error_func(fake_dfs)
+
+    assert result == {'Episodes': [1, 5]}

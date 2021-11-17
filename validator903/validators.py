e10e710b5751564ab25029398a170a7d782d2286
Steve-Far <92323914+Steve-Far@users.noreply.github.com>
2021-11-17 12:39:32 +0000

    Added rule for issue 42 (Error 158)

--- validator903/validators.py
+++ validator903/validators.py
@@ -1,6 +1,28 @@
 import pandas as pd
 from .types import ErrorDefinition
 
+def validate_158():
+    error = ErrorDefinition(
+        code='158',
+        description='If a child has been recorded as receiving an intervention for their substance misuse problem, then the additional item on whether an intervention was offered should be left blank.',
+        affected_fields=['INTERVENTION_RECEIVED','INTERVENTION_OFFERED'],
+    )
+
+    def _validate(dfs):
+        if 'OC2' not in dfs:
+            return {}
+        
+        else:
+            oc2 = dfs['OC2']
+        
+            error_mask = oc2['INTERVENTION_RECEIVED'].astype(str).eq('1') & oc2['INTERVENTION_OFFERED'].notna()
+
+            error_locations = oc2.index[error_mask]
+
+            return{'OC2': error_locations.tolist()}
+
+    return error, _validate
+
 def validate_518():
     error = ErrorDefinition(
         code = '518',

52e5d5e48789ceacb37242942fb10e2459efdb04
Phil Deakin <philip.deakin@manchester.gov.uk>
2021-11-23 14:13:10 +0000

    Added 432

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -3033,4 +3033,27 @@ def test_validate_1012():
 
     assert error_func(fake_dfs_partial) == {'AD1': [3], 'PrevPerm': [4], 'OC2': [5]}
 
+def test_validate_432():
+    fake_data = pd.DataFrame([
+        {'CHILD': '111', 'RNE': 'P', 'DECOM': '01/06/2020', 'REC': 'E2'},  # 0
+        {'CHILD': '111', 'RNE': 'P', 'DECOM': '05/06/2020', 'REC': 'E3'},  # 1
+        {'CHILD': '111', 'RNE': 'T', 'DECOM': '06/06/2020', 'REC': 'E3'},  # 2
+        {'CHILD': '111', 'RNE': 'S', 'DECOM': '08/06/2020', 'REC': pd.NA},  # 3
+        {'CHILD': '222', 'RNE': 'S', 'DECOM': '05/06/2020', 'REC': 'E3'},  # 4
+        {'CHILD': '333', 'RNE': 'B', 'DECOM': '06/06/2020', 'REC': 'E3'},  # 5
+        {'CHILD': '333', 'RNE': 'U', 'DECOM': '10/06/2020', 'REC': pd.NA},  # 6
+        {'CHILD': '444', 'RNE': 'S', 'DECOM': '07/06/2020', 'REC': 'E3'},  # 7
+        {'CHILD': '444', 'RNE': 'S', 'DECOM': '08/06/2020', 'REC': 'E3'},  # 8
+        {'CHILD': '444', 'RNE': 'T', 'DECOM': '09/06/2020', 'REC': 'E3'},  # 9
+        {'CHILD': '444', 'RNE': 'S', 'DECOM': '15/06/2020', 'REC': 'E3'},  # 10
+    ])
+
+    fake_dfs = {'Episodes': fake_data}
+
+    error_defn, error_func = validate_432()
+
+    result = error_func(fake_dfs)
+
+    assert result == {'Episodes': [1, 2, 6, 9]}
+
 

0d6ff62a2a6fa50b0b832c0ef50e7d4afaa76c3e
dezog <LEVEl3399>
2021-12-01 21:36:22 +0000

    log failing errors to file and continue

--- validator903/validator.py
+++ validator903/validator.py
@@ -1,4 +1,5 @@
 import logging
+import datetime
 from typing import Any, List, Dict
 from pandas import DataFrame
 
@@ -8,6 +9,12 @@ from .config import configured_errors
 from .datastore import copy_datastore, create_datastore
 
 logger = logging.getLogger(__name__)
+handler = logging.FileHandler(datetime.datetime.now().strftime('903 Validator -- %d-%m-%Y %H.%M.%S.log'))
+handler.setLevel(logging.DEBUG)
+
+f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(f_format)
+logger.addHandler(handler)
 
 
 class Validator:
@@ -27,10 +34,9 @@ class Validator:
             if error.code in error_codes:
                 logger.info(f'Validating error {error.code}...')
                 ds_copy = copy_datastore(data_store)
-                result: Dict[str, List[Any]] = test_func(ds_copy)
-                for table, values in result.items():
-                    if len(values) > 0:
-                        logger.debug(f"Test {error.code} found {len(values)} errors")
-                        ds_results[table].loc[values, f'ERR_{error.code}'] = True
-
+                try:
+                    result: Dict[str, List[Any]] = test_func(ds_copy)
+                except Exception as e:
+                    logger.exception(f"Error check {error.code} failed to run!")
+                    continue
         return ds_results

7efb196721be199ea8c95edf8bdd77a423dc1e49
Tambe Tabitha Achere <tambetachere@gmail.com>
2021-12-09 15:52:49 +0000

    Added 561

--- validator903/validators.py
+++ validator903/validators.py
@@ -4,6 +4,35 @@ from .datastore import merge_postcodes
 from .types import ErrorDefinition
 from .utils import add_col_to_tables_CONTINUOUSLY_LOOKED_AFTER as add_CLA_column  # Check 'Episodes' present before use!
 
+def validate_561():
+  error = ErrorDefinition(
+    code = '561',
+    description = 'Date of the decision that the child should be placed for adoption this year is the same as that recorded last year but records show that the decision changed, and the child should no longer be placed for adoption last year.',
+    affected_fields = ['DATE_PLACED', 'DATE_PLACED_CEASED', 'REASON_PLACED_CEASED']
+  )
+  def _validate(dfs):
+    if 'PlacedAdoption' not in dfs or 'PlacedAdoption_last' not in dfs:
+      return {}
+    else:
+      placed_adoption = dfs['PlacedAdoption']
+      pa_last = dfs['PlacedAdoption_last']
+
+      # convert dates to appropriate format
+      pa_last['DATE_PLACED'] = pd.to_datetime(pa_last['DATE_PLACED'], format='%d/%m/%Y', errors='coerce')
+      placed_adoption['DATE_PLACED'] = pd.to_datetime(placed_adoption['DATE_PLACED'], format='%d/%m/%Y', errors='coerce')
+
+      # prepare to merge
+      placed_adoption.reset_index(inplace=True)
+      pa_last.reset_index(inplace=True)
+      merged = placed_adoption.merge(pa_last, how='left', on='CHILD', suffixes=['_now', '_last'])
+
+      # If <CURRENT_COLLECTION_YEAR> <DATE_PLACED> is = <CURRENT_COLLECTION_YEAR> -1 <DATE_PLACED> then <CURRENT_COLLECTION_YEAR> -1 <DATE_PLACED_CEASED> and <REASON_PLACED_CEASED> should be Null
+      mask = (merged['DATE_PLACED_now']==merged['DATE_PLACED_last']) & (merged['REASON_PLACED_CEASED_last'].notna() | merged['DATE_PLACED_CEASED_last'].notna())
+      # error locations
+      error_locs =  merged.loc[mask, 'index_now']
+      return {'PlacedAdoption':error_locs.tolist()}
+
+  return error, _validate 
 def validate_165():
   error = ErrorDefinition(
     code = '165',

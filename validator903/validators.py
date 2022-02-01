44dcdfc404bb649e89a535893212974662f6e4d5
dezog <LEVEl3399>
2022-02-01 17:37:59 +0000

    303: compare as numeric

--- validator903/validators.py
+++ validator903/validators.py
@@ -6698,13 +6698,15 @@ def validate_392B():
 
 def validate_303():
     error = ErrorDefinition(
-        code = '303',
-        description = "If date Unaccompanied Asylum-Seeking Child (UASC) status ceased is not null, UASC status must be coded 1.",
+        code='303',
+        description="If date Unaccompanied Asylum-Seeking Child (UASC) status ceased is not null, UASC status must be coded 1.",
         affected_fields=['DUC', 'UASC']
     )
 
     def _validate(dfs):
-        if ('UASC' not in dfs) or ('Header' not in dfs) or ('UASC' not in dfs['Header'].columns):
+        if ('UASC' not in dfs) or ('Header' not in dfs):
+            return {}
+        elif 'UASC' not in dfs['Header'].columns:
             return {}
         else:
             uasc = dfs['UASC']
@@ -6713,14 +6715,18 @@ def validate_303():
             # merge
             uasc.reset_index(inplace=True)
             header.reset_index(inplace=True)
+
             merged = header.merge(uasc, how='left', on='CHILD', suffixes=['_er', '_sc'])
 
+            merged['UASC'] = pd.to_numeric(merged['UASC'], errors='coerce')
+
             # If <DUC> provided, then <UASC> must be '1'
-            error_mask = merged['DUC'].notna() & (merged['UASC'].astype(str)!='1')
+            error_mask = merged['DUC'].notna() & (merged['UASC'] != 1)
 
             uasc_error_locs = merged.loc[error_mask, 'index_sc']
             header_error_locs = merged.loc[error_mask, 'index_er']
             
-            return {'UASC': uasc_error_locs.tolist(), 'Header':header_error_locs.tolist()}
+            return {'UASC': uasc_error_locs.tolist(),
+                    'Header': header_error_locs.tolist()}
 
     return error, _validate

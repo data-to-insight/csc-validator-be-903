1f0ad034fb2f55716ab3e034d6bcd4713c0af05a
dezog <LEVEl3399>
2022-02-14 10:19:56 +0000

    provider info table constructed in ingress and added to metadata in Validator

--- validator903/ingress.py
+++ validator903/ingress.py
@@ -10,10 +10,29 @@ from pandas import DataFrame
 from numpy import nan
 from .types import UploadError, UploadedFile
 from .config import column_names
+from .datastore import la_df, merge_postcodes
 
 import logging
+from time import perf_counter as now
 logger = logging.getLogger(__name__)
 #logger.setLevel(logging.DEBUG)  # uncomment if you want to see debug messages
+
+
+class Timer():
+    def __init__(self):
+        self._prev = None; self._zero = None
+        self.t0
+    @property
+    def t0(self):
+        self._zero = now(); self._prev = self._zero
+        return f'Starting timer'
+    @property
+    def t(self):
+        t = now();  elapsed = t - self._prev; self._prev = t
+        return f'{elapsed:05.2f}s (total {t - self._zero:05.02f}s)'
+
+sc = Timer()
+
 
 class _BufferedUploadedFile(collections.abc.Mapping):
 
@@ -42,7 +61,7 @@ class _BufferedUploadedFile(collections.abc.Mapping):
         pass
 
 
-def read_from_text(raw_files: List[UploadedFile]) -> Tuple[Dict[str, DataFrame], str]:
+def read_from_text(raw_files: List[UploadedFile]) -> Tuple[Dict[str, DataFrame], Dict[str, Union[str, DataFrame]]]:
     """
     Reads from a raw list of files passed from javascript. These files are of
     the form e.g.
@@ -53,6 +72,8 @@ def read_from_text(raw_files: List[UploadedFile]) -> Tuple[Dict[str, DataFrame],
     This function will try to catch most basic upload errors, and dispatch other errors
     to either the csv or xml reader based on the file extension.
     """
+    logger.info(f'Reading from text. {sc.t0}')
+    metadata_extras = {}
 
     CH_uploaded = [f for f in raw_files if f['description'] == 'CH lookup']
     SCP_uploaded = [f for f in raw_files if f['description'] == 'SCP lookup']
@@ -71,38 +92,159 @@ def read_from_text(raw_files: List[UploadedFile]) -> Tuple[Dict[str, DataFrame],
             "from Ofsted into their respective boxes above."
         )
     elif set(num_of_CH_and_SCP) == {1}:
-        (CH_uploaded,) = CH_uploaded
-        (SCP_uploaded,) = SCP_uploaded
-        construct_URN_lookup_table(CH=CH_uploaded, SCP=SCP_uploaded)
+        logger.info(f'Ofsted CH & SCP spreadsheets received - constructing provider info table. {sc.t}')
+        (CH_uploaded, ) = CH_uploaded  # These should both be single-element lists
+        (SCP_uploaded, ) = SCP_uploaded
+        provider_info_df = construct_provider_info_table(CH=CH_uploaded, SCP=SCP_uploaded)
+        metadata_extras['provider_info'] = provider_info_df
+
+        logger.info('created providers table. shape:{1} rows, {0}'.format(*provider_info_df.shape))
+        logger.debug(f'{", ".join(provider_info_df.columns)}')
+        logger.debug(f'providers dtypes: {provider_info_df.dtypes}')
+
+    else:
+        logger.info('Ofsted CH & SCP spreadsheets not loaded - checks involving URN lookup will be skipped.')
 
     raw_files = [f for f in raw_files
                  if f['description'] not in ('CH lookup', 'SCP lookup')]
 
     extensions = list(set([f["name"].split('.')[-1].lower() for f in raw_files]))
+
     if len(raw_files) == 0:
         raise UploadError('No SSDA 903 files uploaded!')
     elif len(extensions) > 1:
         raise UploadError(f'Mix of CSV and XML files found ({extensions})! Please reupload.')
     else:
-        if extensions[0] == 'csv':
-            return read_csvs_from_text(raw_files), 'csv'
-        elif extensions[0] == 'xml':
-            return read_xml_from_text(raw_files[0]['fileText']), 'xml'
+        if extensions == ['csv']:
+            metadata_extras['file_format'] = 'csv'
+            return read_csvs_from_text(raw_files), metadata_extras
+        elif extensions == ['xml']:
+            metadata_extras['file_format'] = 'xml'
+            return read_xml_from_text(raw_files[0]['fileText']), metadata_extras
         else:
             raise UploadError(f'Unknown file type {extensions[0]} found.')
 
 
-def construct_URN_lookup_table(CH, SCP):
-    logger.info('type:' + str(type(CH['fileText'])))
-    logger.info('filtext'+ str(CH['fileText']))
-    logger.info('dir(FILETEXT):''\n' + str(dir(CH['fileText'])))
 
-    # TODO: read only the necessary data; hopefully improve performance
-    CH_ = pd.read_excel(CH['fileText'].tobytes(), sheet_name=None, engine='openpyxl')
-    SCP = pd.read_excel(SCP['fileText'].tobytes(), sheet_name=None, engine='openpyxl')
-    logger.info(f'{type(CH)}, {CH}')
-    logger.info(f'{[i.shape for i in CH.items()]}')
-    return
+def construct_provider_info_table(CH: UploadedFile, SCP: UploadedFile):
+    """
+    inputs:
+    CH (childrens homes) and SCP (social care providers) are either strings containing a path to
+    the excel files, or the files' contents as passed in by the frontend
+
+    returns:
+    provider_info_df is a dataframe
+    (*) it will be stored in dfs['metadata']['provider_info'] if it has been uploaded
+    (*) if it's not there, then  return {} for rules requiring it
+    (*) the columns will be ['URN', 'PLACE_CODES', 'PROVIDER_CODES', 'REG_END', 'POSTCODE', 'LA_CODE']:
+    (*) all cols will be strings (object dtype) except REG_END which will be datetime
+        - URN:
+            merge on this.
+        - PLACE_CODES and PROVIDER_CODES:
+            strings of codes separated by commas: ['PR4', 'PR1,PR2', etc..]
+        - REG_END:
+            date provider should have closed (usually blank) - note its type will already be datetime64
+        - POSTCODE:
+            postcode - upper-cased, spaces removed
+        - LA_CODE_INFERRED and LA_NAME_INFERRED:
+            utla code and utla name - obtained from la_df by matching on provider postcode
+            (!) These may differ from the LA named in the files, either due to formatting, or if they serve
+            different LA to where they are located. Rule descriptions should mention this caveat.
+        - LA_NAME_FROM_FILE
+            LA name from uploaded files.
+    """
+
+    if not isinstance(CH, str):
+        CH_bytes = CH['fileText'].tobytes()
+    else:
+        CH_bytes = CH
+    if not isinstance(SCP, str):
+        SCP_bytes = SCP['fileText'].tobytes()
+    else:
+        SCP_bytes = SCP
+    provider_info_cols = ['URN', 'LA_NAME_FROM_FILE', 'PLACE_CODES', 'PROVIDER_CODES', 'REG_END', 'POSTCODE']
+    logger.info(f'URN lookup bytes recieved. Reading excel files... {sc.t}')
+
+    # read childrens homes file
+    # this sheet contains all columns except the postcode
+    CH_setting = pd.read_excel(CH_bytes, sheet_name=0, engine='openpyxl')
+    logger.debug(f'Reading CH setting info from excel done. cols:{CH_setting.columns} {sc.t}')
+    # from this sheet we need only the postcode
+    CH_address = pd.read_excel(CH_bytes, sheet_name=2, engine='openpyxl')
+    logger.debug(f'Reading CH address info from excel done. cols:{CH_address.columns} {sc.t}')
+
+    CH_cols = ['URN', 'Local Authority', 'Provider Code', 'Provider Placement Code', 'Closed Date', 'Setting Postcode']
+
+    try:
+        CH_address = CH_address[['URN', 'Setting Postcode']]
+    except KeyError:
+        try:
+            CH_address = (
+                CH_address[['URN', 'Setting Address Postcode']]
+                .rename(columns={'Setting Address Postcode': 'Setting Postcode'})
+            )
+        except KeyError:
+            raise UploadError('Failed to find postcode column in Childrens Homes list '
+                              '"Address Details sheet. Expected "Setting Postcode"')
+    try:
+        CH_setting = CH_setting[CH_cols[: -1]]
+    except KeyError:
+        raise UploadError(f'Failed to find required columns in Childrens Homes list "Setting and Inspection Info"'
+                          f' sheet. Expected: {", ".join(CH_cols[: -1])}')
+
+    # combine the postcode with the rest of the columns
+    CH_df = pd.merge(
+        left=CH_setting, right=CH_address,
+        on='URN', how='inner'
+    )
+
+    CH_df['source'] = "CH List"
+    CH_df['Provider Placement Code'] = CH_df['Provider Placement Code'].str.replace('/', ',')
+    provider_info_df = CH_df.rename(columns=dict(zip(CH_cols, provider_info_cols)))
+    del CH_setting, CH_address, CH_df
+    logger.info(f'CH dataframe complete. Creating SCP dataframe {sc.t}')
+
+    # read social care providers file
+    SCP_current = pd.read_excel(SCP_bytes, engine='openpyxl', sheet_name=0)  # current providers
+    logger.debug(f'Reading SCP_current from excel done. cols: {SCP_current.columns}. {sc.t}')
+    SCP_closed = pd.read_excel(SCP_bytes, engine='openpyxl', sheet_name=1)  # closed providers
+    logger.debug(f'Reading SCP_closed from excel done. cols: {SCP_closed.columns}. {sc.t}')
+
+    SCP_cols = ['URN', 'Local authority', 'Placement code', 'Placement provider code', 'Deregistration date', 'Setting postcode']
+    try:
+        SCP_current = SCP_current[[_ for _ in SCP_cols if _ != 'Deregistration date']]
+    except KeyError:
+        missing_cols = set(_ for _ in SCP_cols if _ != "Deregistration date") - set(SCP_current.columns)
+        raise UploadError(f'Failed to find required columns in Social Care Providers list "Current providers"'
+                          f' sheet. Didnt find: {", ".join(missing_cols)}')
+    try:
+        SCP_closed = SCP_closed[SCP_cols]
+    except KeyError:
+        missing_cols = set(SCP_cols) - set(SCP_closed.columns)
+        raise UploadError(f'Failed to find required columns in Social Care Providers list "Providers closed"'
+                          f' sheet. Didnt find: {", ".join(missing_cols)}')
+
+    # concatenate the two SCP sheets and rename the columns
+    SCP_df = pd.concat((SCP_current, SCP_closed)).rename(columns=dict(zip(SCP_cols, provider_info_cols)))
+    SCP_df = SCP_df[SCP_df['URN'].notnull()]
+    SCP_df['source'] = "SCP List"
+
+    # add SCP info to provider_info_df
+    provider_info_df = pd.concat((provider_info_df, SCP_df), ignore_index=True)
+    del SCP_current, SCP_closed, SCP_df
+
+    # standardise postcodes
+    provider_info_df['POSTCODE'] = provider_info_df['POSTCODE'].str.replace(' ', '').str.upper()
+
+    # infer LA based on provider's postcode. this does not necessarily match what's in the file!
+    provider_info_df[['LA_CODE_INFERRED', 'LA_NAME_INFERRED']] = (
+        merge_postcodes(provider_info_df, 'POSTCODE')
+        .merge(la_df, how='left', left_on='laua', right_on='LTLA21CD')
+        .loc[:, ['UTLA21CD', 'UTLA21NM']]
+    )
+
+    logger.info(f'Provider info dataframe successfully created {sc.t}')
+    return provider_info_df
 
 
 def read_files(files: Union[str, Path]) -> List[UploadedFile]:

e24d27a054259d9d72739a364f27feeb8ab309ad
tab1tha <tambe.tabitha@socialfinance.org.uk>
2023-06-02 17:59:35 +0100

    switch on imports so that validation works

--- lac_validator/rules/lac_2022_23/__init__.py
+++ lac_validator/rules/lac_2022_23/__init__.py
@@ -1,5 +1,7 @@
 from pathlib import Path
-# from . import *
 
 __all__ = [p.stem for p in Path(__file__).parent.glob("*.py") if p.stem != "__init__"]
 
+# TODO now this line has to be commented for tests to run and uncommented for validation to run.
+# else it loads the registry twice and says that the rules are duplicates.
+from . import *

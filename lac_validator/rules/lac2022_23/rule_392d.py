5fce9e58957fb0e6ebf67409ac9febfdb8809e7d
SLornieCYC <89449804+SLornieCYC@users.noreply.github.com>
2023-10-05 11:16:21 +0000

    Restore branch changes (adding rule 392d)

---
+++ lac_validator/rules/lac2022_23/rule_392d.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+from lac_validator.datastore import merge_postcodes
+from lac_validator.rule_engine import rule_definition
+
+
+@rule_definition(
+    code="392d",
+    message="Home and placement postcodes should not be same unless the placement type is P1.",
+    affected_fields=["HOME_POST", "PL_POST", "PLACE"],
+)
+def validate(dfs):
+    if "Episodes" not in dfs:
+        return {}
+    else:
+        episodes = dfs["Episodes"]
+
+        not_p1 = episodes[(episodes["PLACE"] != "P1")]
+
+        error_mask = not_p1[not_p1["HOME_POST"] == not_p1["PL_POST"]]
+
+        return {"Episodes": episodes.index[error_mask].tolist()}
+
+
+def test_validate():
+    import pandas as pd
+
+    fake_data = pd.DataFrame(
+        {
+            "PLACE": ["P2", "P1", "P2",],
+            "PL_POST": [
+                "BN20",
+                "BN20",
+                "BN21",
+            ],
+            "HOME_POST": [
+                "BN20",
+                "BN20",
+                "BN20",
+            ],
+        }
+    )
+
+    fake_dfs = {"Episodes": fake_data}
+
+    result = validate(fake_dfs)
+
+    assert result == {
+        "Episodes": [
+            0,
+        ]
+    }

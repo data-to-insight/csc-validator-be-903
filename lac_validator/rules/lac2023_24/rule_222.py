896f76a3b8537f5437737a0ff152f2adaf6515b9
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2023-11-08 13:30:04 +0000

    222 ammended for 23/24 return with test

--- lac_validator/rules/lac2023_24/rule_222.py
+++ lac_validator/rules/lac2023_24/rule_222.py
@@ -9,13 +9,14 @@ from lac_validator.rule_engine import rule_definition
     affected_fields=["URN"],
 )
 def validate(dfs):
+    # If <URN> provided and <URN> not = ‘XXXXXX’, and where
+    # <PL> = ‘P1’; ‘P3’; ‘R1’; ‘R2’; ‘R5’; ‘T0’ ‘T1’; ‘T2’; ‘T3’; ‘T4’ or Z1 then should not be provided
     if "Episodes" not in dfs:
         return {}
     else:
         df = dfs["Episodes"]
         place_code_list = [
             "P1",
-            "P2",
             "P3",
             "R1",
             "R2",
@@ -47,6 +48,7 @@ def test_validate():
             {"PLACE": "T1", "URN": pd.NA},  # 3
             {"PLACE": "R1", "URN": "Whatever"},  # 4  Fail
             {"PLACE": "T2", "URN": "Whatever"},  # 5  Fail
+            {"PLACE": "P2", "URN": "Whatever"},  # 6  Pass
         ]
     )
 

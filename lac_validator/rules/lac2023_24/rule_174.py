e7a48f2b210ae8d3a827f9109473b6f9c43c17d3
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2023-11-08 11:12:45 +0000

    174 updated gender code 1 to F

---
+++ lac_validator/rules/lac2023_24/rule_174.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+from lac_validator.rule_engine import rule_definition
+
+
+@rule_definition(
+    code="174",
+    message="Mother's child date of birth is recorded but gender shows that the child is a male.",
+    affected_fields=["SEX", "MC_DOB"],
+)
+def validate(dfs):
+    if "Header" not in dfs:
+        return {}
+    else:
+        header = dfs["Header"]
+
+        child_is_male = header["SEX"] == "F"
+        mc_dob_recorded = header["MC_DOB"].notna()
+
+        error_mask = child_is_male & mc_dob_recorded
+
+        validation_error_locations = header.index[error_mask]
+
+        return {"Header": validation_error_locations.to_list()}
+
+
+def test_validate():
+    import pandas as pd
+
+    fake_data = pd.DataFrame(
+        {
+            "SEX": ["F", "F", "2", "2", 1, 1],
+            "MC_DOB": [pd.NA, "19/02/2010", pd.NA, "19/02/2010", pd.NA, "19/02/2010"],
+        }
+    )
+
+    fake_dfs = {"Header": fake_data}
+
+    result = validate(fake_dfs)
+
+    assert result == {"Header": [1]}

d518e64b534bd28cb92d8e3c343814791aadb881
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2023-12-11 08:12:51 +0000

    fix rule length test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,7 +11,7 @@ from lac_validator.utils import (
 
 def test_get_year_ruleset():
     registry = get_year_ruleset("2023")
-    assert len(registry) == 291
+    assert len(registry) == 292
     assert isinstance(registry, dict)
     assert isinstance(list(registry.values())[1], RuleDefinition)
 

9b663b524a636e106fa87514faca8f53e4b2f16e
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2023-12-18 14:57:45 +0000

    SW09STG2

---
+++ lac_validator/rules/lac2023_24/rule_SW09STG2.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+from lac_validator.rule_engine import rule_definition
+
+
+@rule_definition(
+    code="SW09STG2",
+    message="Social worker episode appears to have lasted for less than 24 hours.",
+    affected_fields=["SW_DECOM", "SW_DEC"],
+)
+def validate(dfs):
+    if "SWEpisodes" not in dfs:
+        return {}
+    else:
+        df = dfs["SWEpisodes"]
+
+        error_rows = df[df["SW_DEC"] == df["SW_DECOM"]].index
+
+        return {"SWEpisodes": error_rows.tolist()}
+
+
+def test_validate():
+    import pandas as pd
+
+    fake_data = pd.DataFrame(
+        [
+            {"CHILD": "1", "SW_DEC": "01/01/2000", "SW_DECOM": "01/01/2000"},  # fail
+            {"CHILD": "1", "SW_DEC": "01/01/2000", "SW_DECOM": "02/01/2000"},
+            {"CHILD": "2", "SW_DEC": "01/01/1901", "SW_DECOM": "01/01/1901"},
+        ]
+    )
+
+    fake_dfs = {"SWEpisodes": fake_data}
+
+    result = validate(fake_dfs)
+
+    assert result == {"SWEpisodes": [0, 2]}

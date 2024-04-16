a2d64a6d4c01d19dd93033c00d4c724be7cb6f55
SLornieCYC <89449804+SLornieCYC@users.noreply.github.com>
2024-04-16 10:38:31 +0000

    Update to use utility valid_date function

--- lac_validator/rules/lac2023_24/rule_SW05STG1.py
+++ lac_validator/rules/lac2023_24/rule_SW05STG1.py
@@ -12,50 +12,13 @@ def validate(dfs):
     if "SWEpisodes" not in dfs:
         return {}
     else:
+        import lac_validator.rules.rule_utils
+
         df = dfs["SWEpisodes"]
 
         df = df[df["SW_DEC"].notna()].copy()
 
-        # function to check that date is of the right format
-        def valid_date(dte):
-            try:
-                lst = dte.split("/")
-            except AttributeError:
-                return pd.NaT
-            # Preceding block checks for the scenario where the value passed in is nan/naT
-
-            # date should have three elements
-            if len(lst) != 3:
-                return pd.NaT
-
-            z_list = ["ZZ", "ZZ", "ZZZZ"]
-            # We set the date to the latest possible value to avoid false positives
-            offset_list = [
-                pd.DateOffset(months=1, days=-1),
-                pd.DateOffset(years=1, days=-1),
-                None,
-            ]
-            # that is, go to the next month/year and take the day before that
-            date_bits = []
-
-            for i, zeds, offset in zip(lst, z_list, offset_list):
-                if i == "ZZ":
-                    i = "01"
-                    offset_to_use = offset
-                elif i == "ZZZZ":
-                    i = "2000"
-                date_bits.append(i)
-
-            as_datetime = pd.to_datetime(
-                "/".join(date_bits), format="%d/%m/%Y", errors="coerce"
-            )
-            try:
-                as_datetime += offset_to_use
-            except NameError:  # offset_to_use only defined if needed
-                pass
-            return as_datetime
-
-        df["SW_DEC_dt"] = df["SW_DEC"].apply(valid_date)
+        df["SW_DEC_dt"] = df["SW_DEC"].apply(lac_validator.rules.rule_utils.valid_date)
 
         error_rows = df[df["SW_DEC_dt"].isna()].index
 

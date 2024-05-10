80fb9be218c65b097bd97d4bbcb6728a8e80ad29
SLornieCYC <89449804+SLornieCYC@users.noreply.github.com>
2024-05-10 09:30:40 +0000

    Added more test data

--- lac_validator/rules/lac2023_24/rule_SW12STG2.py
+++ lac_validator/rules/lac2023_24/rule_SW12STG2.py
@@ -97,50 +97,92 @@ def test_validate():
             {
                 "CHILD": 1,
                 "DECOM": "01/01/2024",
-                "DEC": "01/02/2024",
-                "REC": "Q",
-                "RNE": "Q",
-                "LS": "Q",
+                "DEC": pd.NA,
+                "REC": pd.NA,
+                "RNE": "S",
+                "LS": "C1",
             },  # Pass, has SW EP
             {
-                "CHILD": 1,
-                "DECOM": "02/02/2024",
-                "DEC": "01/03/2024",
-                "REC": "Q",
-                "RNE": "Q",
-                "LS": "Q",
-            },  # Pass has SW EP
-            {
-                "CHILD": 1,
-                "DECOM": "02/03/2024",
-                "DEC": "03/03/2024",
-                "REC": "Q",
-                "RNE": "Q",
-                "LS": "Q",
-            },  # FAIL, no SW EP
-            {
-                "CHILD": 1,
-                "DECOM": "04/03/2024",
-                "DEC": pd.NA,
-                "REC": "Q",
-                "RNE": "Q",
-                "LS": "Q",
-            },  # Pass, not closed?
-            {
-                "CHILD": 2,  # passes, no dec inyear
+                "CHILD": 2,
                 "DECOM": "01/01/2024",
                 "DEC": pd.NA,
                 "REC": pd.NA,
-                "RNE": pd.NA,
-                "LS": pd.NA,
-            },
+                "RNE": "S",
+                "LS": "C1",
+            },  # FAIL, no SW EP
+            {
+                "CHILD": 3,
+                "DECOM": "01/01/2024",
+                "DEC": pd.NA,
+                "REC": pd.NA,
+                "RNE": "S",
+                "LS": "V4",
+            },  # Pass, no SW EP but V4 (exempt)
+            {
+                "CHILD": 4,
+                "DECOM": "01/01/2024",
+                "DEC": "10/01/2024",
+                "REC": "X1",
+                "RNE": "P",
+                "LS": "C1",
+            },  # Pass, no SW EP but not a start or end placement
+            {
+                "CHILD": 4,
+                "DECOM": "10/01/2024",
+                "DEC": pd.NA,
+                "REC": pd.NA,
+                "RNE": "P",
+                "LS": "C1",
+            },  # Pass, no SW EP but not a start or end placement
+            {
+                "CHILD": 5,
+                "DECOM": "01/01/2024",
+                "DEC": "10/01/2024",
+                "REC": "X1",
+                "RNE": "S",
+                "LS": "C1",
+            },  # FAIL no SW EP (started after end)
+            {
+                "CHILD": 5,
+                "DECOM": "10/01/2024",
+                "DEC": "01/03/2024",
+                "REC": "E4a",
+                "RNE": "P",
+                "LS": "C1",
+            },  # Pass has SW EP
+            {
+                "CHILD": 6,
+                "DECOM": "01/01/2024",
+                "DEC": "10/01/2024",
+                "REC": "X1",
+                "RNE": "S",
+                "LS": "C1",
+            },  # Pass has SW EP
+            {
+                "CHILD": 6,
+                "DECOM": "10/01/2024",
+                "DEC": "01/03/2024",
+                "REC": "E4a",
+                "RNE": "P",
+                "LS": "C1",
+            },  # Pass has SW EP
+            {
+                "CHILD": 7,
+                "DECOM": "01/04/2024",
+                "DEC": pd.NA,
+                "REC": "X1",
+                "RNE": "S",
+                "LS": "C1",
+            },  # Edge case? FAIL but technically think should be fine if the SW_DEC (or no DEC) was after EPI DECOM (which we're not checking here)
         ]
     )
 
     sw_eps = pd.DataFrame(
         [
             {"CHILD": 1, "SW_DECOM": "01/01/2024"},
-            {"CHILD": 1, "SW_DECOM": "02/02/2024"},
+            {"CHILD": 5, "SW_DECOM": "01/02/2024"},
+            {"CHILD": 6, "SW_DECOM": "01/02/2024"},
+            {"CHILD": 7, "SW_DECOM": "01/02/2024"},
         ]
     )
 
@@ -152,4 +194,4 @@ def test_validate():
 
     print(result)
 
-    assert result == {"Episodes": [2]}
+    assert result == {"Episodes": [1, 4, 9]}

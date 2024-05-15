c3a79208f8753e7340274441a043a1860acc5c74
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2024-05-15 07:48:49 +0000

    SW12STG2 working, main merged back and test utils updated

--- lac_validator/rules/lac2023_24/rule_SW12STG2.py
+++ lac_validator/rules/lac2023_24/rule_SW12STG2.py
@@ -16,13 +16,6 @@ def validate(dfs):
         SWE = dfs["SWEpisodes"]
         epi = dfs["Episodes"]
 
-        collection_start = pd.to_datetime(
-            dfs["metadata"]["collection_start"], format="%d/%m/%Y"
-        )
-        collection_end = pd.to_datetime(
-            dfs["metadata"]["collection_end"], format="%d/%m/%Y"
-        )
-
         SWE["SW_DECOM"] = pd.to_datetime(
             SWE["SW_DECOM"], format="%d/%m/%Y", errors="coerce"
         )
@@ -30,61 +23,20 @@ def validate(dfs):
         epi["DEC"] = pd.to_datetime(epi["DEC"], format="%d/%m/%Y", errors="coerce")
         epi["DECOM"] = pd.to_datetime(epi["DECOM"], format="%d/%m/%Y", errors="coerce")
 
-        # This assumes that missing SW Episodes are only ones not added to the end, not middle ones
-        epi.sort_values(["CHILD", "DECOM"], ascending=True, inplace=True)
-        epi["EPI_ID"] = epi.groupby("CHILD").cumcount()
-        SWE.sort_values(["CHILD", "SW_DECOM"], ascending=True, inplace=True)
-        SWE["EPI_ID"] = SWE.groupby("CHILD").cumcount()
+        epi = epi.reset_index()
 
-        epi = epi[(epi["DEC"] >= collection_start) & (epi["DEC"] <= collection_end)]
+        epi = epi[
+            (~epi["LS"].isin(["V3", "V4"]))
+            & ((epi["RNE"] == "S") | (epi["REC"] != "X1"))
+        ]
 
-        df = pd.merge(epi, SWE, how="left", on=["CHILD", "EPI_ID"])
+        df = pd.merge(epi, SWE, how="left", on=["CHILD"])
 
-        # finding non-respite periods of care
-        swdecom_before_dec_x1 = (
-            (df["SW_DECOM"] <= df["DEC"]) | df["SW_DECOM"].isna()
-        ) & (df["REC"] != "X1")
-        swdecom_after_decom_s = (
-            (df["SW_DECOM"] >= df["DECOM"]) | df["SW_DECOM"].isna()
-        ) & (df["RNE"] == "S")
-        not_V3_V4 = ~df["LS"].isin(["V3", "V4"])
+        # finding episodes that start or end care and don't have a corresponding SWE episode
+        swdecom_before_dec_nx1 = ((df["SW_DECOM"] <= df["DEC"])) & (df["REC"] != "X1")
+        swdecom_after_decom_s = ((df["SW_DECOM"] >= df["DECOM"])) & (df["RNE"] == "S")
 
-        # If any of these are true, the child was not continously looked after
-        not_continous = df[swdecom_before_dec_x1 | swdecom_after_decom_s | not_V3_V4]
-
-        # in_care_at_some_point = not_continous.groupby("CHILD")["DEC_IN_YEAR"].transform("max")
-        # not_in_care_at_some_point = not_continous.groupby("CHILD")[
-        #     "implies_not_continuous"
-        # ].transform("max")
-
-        # # If CONTINOUSLY LOOKED AFTER == FALSE, there are previous episodes of care
-        # not_continous["CONTINUOUSLY_LOOKED_AFTER"] = (in_care_at_some_point == True) & (
-        #     not_in_care_at_some_point == False
-        # )
-
-        # finding failing rows, overwriting df variable
-        # df = pd.merge(epi, SWE, how="left", on="CHILD")
-
-        # swdecom_before_dec = df[df["SW_DECOM"] < df["DEC"]]["CHILD"].tolist()
-
-        # error_rows = df[
-        #     (df["CONTINUOUSLY_LOOKED_AFTER"] == True)
-        #     & ~(df["CHILD"].isin(swdecom_before_dec))
-        # ]["index"].tolist()
-
-        # not_continous.sort_values(["CHILD", "DECOM", "SW_DECOM"], ascending=[True, True, True], inplace=True)
-        # print(not_continous)
-        # not_continous.drop_duplicates(["CHILD", "SW_DECOM"], inplace=True, keep="first")
-        # print(not_continous)
-
-        # Ensuring we only send rows for which there is no SW Episode
-        # decom_after_dec = not_continous[not_continous['SW_DECOM'] > not_continous["DEC"]]
-
-        # print(decom_after_dec)
-
-        error_rows = not_continous[not_continous["SW_DECOM"].isna()].index
-
-        # error_rows = df[df["CHILD"].isin(errors)].index
+        error_rows = df[~swdecom_before_dec_nx1 & ~swdecom_after_decom_s]["index"]
 
         return {"Episodes": error_rows.tolist()}
 
@@ -186,9 +138,7 @@ def test_validate():
         ]
     )
 
-    metadata = {"collection_start": "01/04/2023", "collection_end": "31/03/2024"}
-
-    fake_dfs = {"SWEpisodes": sw_eps, "Episodes": epi, "metadata": metadata}
+    fake_dfs = {"SWEpisodes": sw_eps, "Episodes": epi}
 
     result = validate(fake_dfs)
 

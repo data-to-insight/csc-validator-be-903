277e31a61a5419a95b116e1a257db33bbcdea9f8
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2024-11-19 14:01:59 +0000

    fix lint

--- lac_validator/rules/lac2024_25/rule_231.py
+++ lac_validator/rules/lac2024_25/rule_231.py
@@ -20,26 +20,34 @@ def validate(dfs):
             dfs["metadata"]["collection_end"], format="%d/%m/%Y", errors="coerce"
         )
 
-        df['DOB'] = pd.to_datetime(df["DOB"], format="%d/%m/%Y", errors="coerce")
-        
-        under_11_uasc = df[((df["DOB"] > collection_end - pd.DateOffset(years=11)  )) & (df['UASC_STATUS'].astype(str) == "1")].index
-        
+        df["DOB"] = pd.to_datetime(df["DOB"], format="%d/%m/%Y", errors="coerce")
+
+        under_11_uasc = df[
+            ((df["DOB"] > collection_end - pd.DateOffset(years=11)))
+            & (df["UASC_STATUS"].astype(str) == "1")
+        ].index
+
         return {"Header": under_11_uasc.tolist()}
 
 
 def test_validate():
     import pandas as pd
 
-    fake_data = pd.DataFrame([{"DOB": "01/04/2014", "UASC_STATUS": "1"}, # under 11 uasc, fails  
-                            {"DOB": "31/03/2014", "UASC_STATUS": "1"}, # 11 day of collection end, uasc, according to stric rule logic, passes
-                            {"DOB": "01/04/2014", "UASC_STATUS": "0"},
-                            {"DOB": "30/03/2014", "UASC_STATUS": "1"}]) # over 11, uasc, passes
-    
+    fake_data = pd.DataFrame(
+        [
+            {"DOB": "01/04/2014", "UASC_STATUS": "1"},  # under 11 uasc, fails
+            {
+                "DOB": "31/03/2014",
+                "UASC_STATUS": "1",
+            },  # 11 day of collection end, uasc, according to stric rule logic, passes
+            {"DOB": "01/04/2014", "UASC_STATUS": "0"},
+            {"DOB": "30/03/2014", "UASC_STATUS": "1"},
+        ]
+    )  # over 11, uasc, passes
 
-    fake_metadata = {"collection_end":"31/03/2025"}
+    fake_metadata = {"collection_end": "31/03/2025"}
 
-    fake_dfs = {"Header":fake_data,
-                "metadata":fake_metadata}
+    fake_dfs = {"Header": fake_data, "metadata": fake_metadata}
 
     result = validate(fake_dfs)
 
